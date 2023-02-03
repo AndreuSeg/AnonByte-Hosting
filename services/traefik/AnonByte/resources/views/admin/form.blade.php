f914f2e21fbd2171c85dd68252de986b738d488c
root <root@AnonByte.com>
2023-02-03 23:47:57 +0100

    Traefik instalado correctamente

---
+++ services/traefik/AnonByte/resources/views/admin/form.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.generals.head')
+@section('tittle', 'Editar usuario {{ $user->name }}')
+
+@section('main')
+    <div class="padre mt-40 w-full">
+        <h2>Edit User {{ $user->name }}</h2>
+        <form class="user-edit gap-3 p-4 w-2/6" method="POST" action="{{ route('admin.users.save-user', ['id' => $id]) }}">
+            @csrf
+            <div class="textbox">
+                <label class="mt-2 ml-2" for="name">Nombre</label>
+                <input class="pt-2 pb-2 pl-4 pr-4 rounded" type="name" name="name" value="{{ $user->name }}">
+            </div>
+            <div class="textbox">
+                <label class="mt-2 ml-2" for="latname">last Name</label>
+                <input class="pt-2 pb-2 pl-4 pr-4 rounded" type="name" name="lastname" value="{{ $user->lastname }}">
+            </div>
+            <div class="textbox">
+                <label class="mt-2 ml-2" for="username">Username</label>
+                <input class="pt-2 pb-2 pl-4 pr-4 rounded" type="name" name="username" value="{{ $user->username }}">
+            </div>
+            <div class="textbox">
+                <label class="mt-2 ml-2" for="email">Email</label>
+                <input class="pt-2 pb-2 pl-4 pr-4 rounded" type="email" name="email" value="{{ $user->email }}">
+            </div>
+            <button class="save-button text-center text-white p-3 rounded" type="submit">Guardar</button>
+        </form>
+    </div>
+@endsection
---
+++ services/traefik/AnonByte/resources/views/dashboard/generals/head.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    {{-- Fin  --}}
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="icon" href="{{ asset('images/logo.png') }}">
+    {{-- @vite('resources/css/app.css') --}}
+    <title>{{ env('APP_NAME') }} | @yield('tittle', 'Default')</title>
+</head>
+
+<body>
+    <div class="padre mt-0 mb-0 ml-8 mr-8 gap-7">
+        <aside class="bg-slate-200">
+            @section('aside')
+                @include('dashboard.generals.aside')
+            @show
+        </aside>
+        {{-- END ASIDE --}}
+        <main>
+            @yield('main')
+        </main>
+    </div>
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+    {{-- Fin --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+</body>
+
+</html>
